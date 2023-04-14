63c8b559b3e274d4b411818011726a78f24fb9a4
Sheomight <nikita-skop@yandex.ru>
2023-04-14 14:30:56 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let menuButton = document.querySelector('.menu__btn');
+let menu = document.querySelector('.menu');
+
+
+
+
+menuButton.addEventListener('click', () => {
+    let header = document.querySelector('.menu').parentNode;
+    let menu = document.querySelector('.menu');
+
+    if (menu.className === "menu") {
+        menu.classList.add("menu_responsive");
+        header.style = 'flex-wrap: wrap; flex-direction: column';
+    } else {
+        menu.className = "menu";
+        header.style = '';
+    }
+});
+
+
